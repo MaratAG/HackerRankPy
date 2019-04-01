16ce7c8bdd9bad22e2be84e4b2cc8d45228fcbc3
maratag <marat@genatulin.com>
2019-04-01 21:19:50 +0300

    Complete task Strings 4

--- HR_PY_Strings_3.py
+++ HR_PY_Strings_3.py
@@ -1,18 +1,21 @@
-# HackerRank Strings 3 String What's your name
+# HackerRank Strings 4 Mutations
 
 
-def problem_solution(first_name, last_name):
+def problem_solution(current_string, parametres):
     # Task function
-    print('Hello {} {}! You just delved into python.'.
-          format(first_name, last_name))
+    index_of_string = int(parametres[0])
+    symbol_for_change = parametres[1]
+
+    new_string = current_string[:index_of_string] + symbol_for_change + current_string[index_of_string + 1:]
+    print(new_string)
 
 
 def main():
     # Initialization
-    first_name = input().strip()
-    last_name = input().strip()
+    current_string = input().strip()
+    parametres = list(input().split())
 
-    problem_solution(first_name, last_name)
+    problem_solution(current_string, parametres)
 
 
 main()
