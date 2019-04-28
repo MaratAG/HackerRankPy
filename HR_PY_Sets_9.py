c87fc2382aacf789ff0807a1c80ab8a7abb5e106
maratag <marat@genatulin.com>
2019-04-28 14:21:22 +0300

    Complete task Sets 9

---
+++ HR_PY_Sets_9.py
@@ -0,0 +1,30 @@
+"""HackerRank Sets 9 Set.symmetric_difference() operation"""
+
+
+def problem_solution(set_n, set_b):
+    # Task function
+    print(len(set_n.symmetric_difference(set_b)))
+
+
+def main():
+    # Initialization
+    n = 0
+    b = 0
+    min_nb = 0
+    max_nb = 1000
+
+    while not n > min_nb and n < max_nb:
+        n = int(input())
+
+    set_n = set(map(int, input().split()))
+
+    while not b > min_nb and b < max_nb:
+        b = int(input())
+
+    set_b = set(map(int, input().split()))
+
+    problem_solution(set_n, set_b)
+
+
+if __name__ == '__main__':
+    main()
