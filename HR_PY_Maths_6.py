ca653c985dc3503849fc29b1d05ca01a052df3eb
maratag <marat@genatulin.com>
2019-05-03 13:48:31 +0300

    Complete task Math 6

---
+++ HR_PY_Maths_6.py
@@ -0,0 +1,20 @@
+""""HackerRank Maths 6 Integers Come In All sizes"""
+
+
+def problem_solution(a, b, c, d):
+    # Task function
+    print(pow(a, b) + pow(c, d))
+
+
+def main():
+    # Initialization
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+
+    problem_solution(a, b, c, d)
+
+
+if __name__ == '__main__':
+    main()
