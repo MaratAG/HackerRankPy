7cefc1b5d865419ccd00365b237e5f902e02618d
MaratAG <marat@genatulin.com>
2019-05-11 16:02:04 +0300

    Task Itertools 7 completed

--- HR_PY_Itertools_7.py
+++ HR_PY_Itertools_7.py
@@ -3,17 +3,18 @@
 
 from itertools import product
 
-def problem_solution(N):
+
+def problem_solution(N, M):
     # Task function
-    print(N)
+    results = map(lambda x: sum(i ** 2 for i in x) % M, product(*N))
+    print(max(results))
 
 
 def main():
     # Initialization
     K, M = map(int, input().split())
-    N = (list(map(int, input().split())) for _ in range(K))
-
-    problem_solution(N)
+    N = [list(map(int, input().split())) for _ in range(K)]
+    problem_solution(N, M)
 
 
 if __name__ == '__main__':
